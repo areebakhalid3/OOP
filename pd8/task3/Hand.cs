ead4d2d39f5b44c7b1e7c76e5ff98d14b08ac163
areebakhalid3 <khalidareeba3@gmail.com>
2025-04-13 21:09:40 +0500

    hi

--- pd8/task3/Hand.cs
+++ pd8/task3/Hand.cs
@@ -28,20 +28,6 @@ namespace task3
             }
         }
 
-        public void RemoveCard(Card c)
-        {
-            if (c != null && cards.Contains(c))
-            {
-                cards.Remove(c);
-            }
-        }
-        public void removeCard(int position) //removes from specific position
-        {
-            if (position > 0 && position < cards.Count())
-            {
-               cards.RemoveAt(position);
-            }
-        }
         public int getCardCount()
         {
             return cards.Count();
@@ -56,62 +42,8 @@ namespace task3
             return null;
         }
 
-        public void sortBySuit()
-        {
-            for (int i = 0; i < cards.Count - 1; i++)
-            {
-                for (int j = i + 1; j < cards.Count; j++)
-                {
-                    Card c1 = cards[i];
-                    Card c2 = cards[j];
 
-                    if (c1.getSuit() > c2.getSuit())
-                    {
-                        Card temp = cards[i];
-                        cards[i] = cards[j];
-                        cards[j] = temp;
-                    }
-                    else if (c1.getSuit() == c2.getSuit())
-                    {
-                        if (c1.getValue() > c2.getValue())
-                        {
-                            Card temp = cards[i];
-                            cards[i] = cards[j];
-                            cards[j] = temp;
-                        }
-                    }
-                }
-            }
-        }
-
-        public void sortByValue()
-        {
-            for (int i = 0; i < cards.Count - 1; i++)
-            {
-                for (int j = i + 1; j < cards.Count; j++)
-                {
-                    Card c1 = cards[i];
-                    Card c2 = cards[j];
-
-                    if (c1.getValue() > c2.getValue())
-                    {
-                        Card temp = cards[i];
-                        cards[i] = cards[j];
-                        cards[j] = temp;
-                    }
-                    else if (c1.getValue() == c2.getValue())
-                    {
-                        if (c1.getSuit() > c2.getSuit())
-                        {
-                            Card temp = cards[i];
-                            cards[i] = cards[j];
-                            cards[j] = temp;
-                        }
-                    }
-                }
-            }
-        }
-
+   
 
     }
 }
